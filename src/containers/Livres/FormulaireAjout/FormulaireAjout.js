4beb03b6938a4e1ea95423655b8bdc0be24b9201
Abou78 <abou.diallo2021@campus-eni.fr>
2022-12-04 11:58:14 +0100

    modification du formulaire

--- src/containers/Livres/FormulaireAjout/FormulaireAjout.js
+++ src/containers/Livres/FormulaireAjout/FormulaireAjout.js
@@ -1,10 +1,48 @@
 import React, {Component} from "react";
+import Bouton from "../../../components/Bouton/Bouton";
 
 class FormulaireAjout extends Component{
+    state = {
+        titreSaisi : "",
+        auteurSaisi : "",
+        nbPagesSaisie: "",
+    }
     render(){
         return (
             <>
                 <h2 className="text-center text-primary" style={{fontFamily:'Sigmar One'}}>Affichage du formulaire d'ajout</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="titre">Titre du livre</label>
+                        <input type="text"
+                               className="form-control"
+                               id="titre"
+                               value={this.state.titreSaisi}
+                               onChange={(event) => this.setState({titreSaisi:event.target.value})}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="auteur">Auteur</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="auteur"
+                            value={this.state.auteurSaisi}
+                            onChange={(event) => this.setState({auteurSaisi:event.target.value})}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="nbPages">Nombre de pages</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="nbPages"
+                            value={this.state.nbPagesSaisie}
+                            onChange={(event) => this.setState({nbPagesSaisie:event.target.value})}
+                        />
+                    </div>
+                    <Bouton typeBtn="btn-primary">Valider</Bouton>
+                </form>
             </>
         );
     }
